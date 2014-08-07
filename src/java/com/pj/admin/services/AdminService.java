c5dfb9897d1c4b0b1db380ce4eb43a7f99b9f327
pengju114 <luzhenwen@lzw.local>
2014-08-07 23:53:29 +0800

    00000

--- src/java/com/pj/admin/services/AdminService.java
+++ src/java/com/pj/admin/services/AdminService.java
@@ -6,15 +6,15 @@ package com.pj.admin.services;
 
 import com.pj.admin.beans.AdminUser;
 import com.pj.admin.beans.Role;
-import com.pj.jdbc.core.JdbcTemplate;
 import com.pj.jdbc.core.ResultList;
+import com.pj.jdbc.services.BaseService;
 import org.apache.log4j.Logger;
 
 /**
  *
  * @author lzw
  */
-public class AdminService extends JdbcTemplate {
+public class AdminService extends BaseService {
     public static final String ADMIN_TABLE="t_admin";
     public static final String ROLE_TABLE="t_role";
     
@@ -23,7 +23,7 @@ public class AdminService extends JdbcTemplate {
     public AdminUser findAdminUserByNameAndPassword(String name,String pwd){
         String sql="select * from "+ADMIN_TABLE+" as a where a.admin_name=? and a.admin_password=?";
         try {
-            AdminUser user=querySingle(sql, new Object[]{name,pwd}, AdminUser.class);
+            AdminUser user=getJdbcTemplate().querySingle(sql, new Object[]{name,pwd}, AdminUser.class);
             return user;
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
@@ -32,16 +32,16 @@ public class AdminService extends JdbcTemplate {
     }
     
     public ResultList<AdminUser> listAdmins(int startRow,int rowCount){
-        return  executeQuery("select * from "+ADMIN_TABLE,null, startRow, rowCount, AdminUser.class);
+        return  getJdbcTemplate().executeQuery("select * from "+ADMIN_TABLE,null, startRow, rowCount, AdminUser.class);
     }
     
     public ResultList<AdminUser> listAdmins(String name,int startRow,int rowCount){   
         name = "%"+name+"%";
-        return  executeQuery("select * from "+ADMIN_TABLE+" a where a.admin_name like ? or a.admin_real_name like ?",new String[]{name,name}, startRow, rowCount, AdminUser.class);
+        return  getJdbcTemplate().executeQuery("select * from "+ADMIN_TABLE+" a where a.admin_name like ? or a.admin_real_name like ?",new String[]{name,name}, startRow, rowCount, AdminUser.class);
     }
     
     public ResultList<Role> listRoles(){
         String sql = "select * from "+ROLE_TABLE;
-        return executeQuery(sql, Role.class);
+        return getJdbcTemplate().executeQuery(sql, Role.class);
     }
 }
